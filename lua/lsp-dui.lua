a6e4cdf8439a8d5cd77a73c62e1a7c4f124a536e
kokaito <kokaito.git@gmail.com>
2025-10-27 21:48:26 +0100

    v0.0.1e - metatable for app so setup() is never required

--- lua/lsp-dui.lua
+++ lua/lsp-dui.lua
@@ -10,4 +10,16 @@ M.setup = function(opts)
   M.app:start()
 end
 
+-- Metatable. Si no se definen opts = ... entonces setup() no es invocado automáticamente
+-- por este motivo hacemos que al tratar de acceder a M.app se invoque setup() si es que
+-- M.app no existe aún.
+setmetatable(M, {
+  __index = function(table, key)
+    if key == "app" and not rawget(table, "app") then
+      M.setup() -- Crea M.app con opciones por defecto
+    end
+    return rawget(table, key)
+  end,
+})
+
 return M
