398c8286e7fab178bcf67de98c3c46f577992ead
kokaito <kokaito.git@gmail.com>
2025-10-27 15:45:06 +0100

    Original init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,11 @@
--- -- init.lua
--- local M = {}
--- local DuiApp = require "lsp-dui.app"
---
--- ---@param opts DuiAppOpts? Opciones de configuración
--- function M.setup(opts)
---   local app = DuiApp:new(opts)
---   app:start()
--- end
---
--- return M
+-- init.lua
+local M = {}
+local DuiApp = require "lsp-dui.app"
 
-local DuiApp = require("lsp-dui.dui_app")
-return {
-  config = function(plugin)
-    local app = DuiApp(plugin.opts)
-    app:start()
-  end,
-}
+---@param opts DuiAppOpts? Opciones de configuración
+function M.setup(opts)
+  local app = DuiApp:new(opts)
+  app:start()
+end
 
+return M
